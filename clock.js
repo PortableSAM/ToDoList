82d906e5f0bb31407fef875217e715616652a47d
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-09-16 17:01:27 +0900

    clockInsert

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".clock"),
+    clockTitle = clockContainer.querySelector("h2");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    const days = date.getDate();
+    const years = date.getFullYear();
+    const month = date.getMonth() + 1;
+    clockTitle.innerText = `${days}. ${month}. ${years}, ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
